ed3b6798811c45969d166e9204b09202ef98e26e
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-10-25 17:43:41 +0300

    lesson 1.3 preview

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import {useEffect, useState} from "react";
 
 const App = () => {
-  return (
-      <div>
-          App
-      </div>
-  );
+    let [counter, setCounter] = useState(0);
+    // const [users, setUsers] = useState([]);
+    const onClickDecrement = () => {
+        counter--;
+        setCounter(counter);
+    }
+    const onCLickIncrement = () => {
+        counter++;
+        setCounter(counter);
+    }
+
+    // useEffect( () => {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(value => value.json())
+    //         .then(value => {
+    //             console.log('asd')
+    //             setUsers(value)
+    //         })
+    // }, [])
+
+    return (
+        <div className="App">
+            {/*{*/}
+            {/*    users.map(value => (*/}
+            {/*        <div key={value.id}>{value.name}</div>*/}
+            {/*    ))*/}
+            {/*}*/}
+            <h1>counter: {counter}</h1>
+            <button onClick={onCLickIncrement}>+</button>
+            <button onClick={onClickDecrement}>-</button>
+        </div>
+    );
+
 };
 
 export {App};
