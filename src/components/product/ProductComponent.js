eacdd658917d5ceabd53007fe3c476869ffe0cd1
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-10-25 16:31:40 +0300

    innit commit 1

---
+++ src/components/product/ProductComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProductComponent.css';
+
+const ProductComponent = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ProductComponent;
