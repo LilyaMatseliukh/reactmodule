cc5acaec26bb53f10be42252a93df3821c70e5c0
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-11-02 19:18:34 +0200

    lesson2

--- src/components/UsersContainer/Users/Users.js
+++ src/components/UsersContainer/Users/Users.js
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
 import css from './Users.module.css';
 import User from "../User/User";
 
@@ -7,9 +9,16 @@ const Users = () => {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res => res.json())
-            .then(value => setUsers(value))
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        //     .then(res => res.json())
+        //     .then(value => setUsers(value))
+
+        // axios.get('https://jsonplaceholder.typicode.com/users')
+        //     .then(res => res.data)
+        //     .then(value => setUsers(value))
+
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(({data}) => setUsers(data))
     }, [])
 
     return (
