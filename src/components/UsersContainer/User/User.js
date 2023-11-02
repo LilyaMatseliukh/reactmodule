90ae3cef1866f10db0ace6e1f16add476f4c3cde
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-11-02 14:57:18 +0200

    lesson2

---
+++ src/components/UsersContainer/User/User.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import css from './User.module.css';
+const User = ({user}) => {
+    const {id, name} = user;
+
+    return (
+        <div className={css.User}>
+            <div>id: {id}</div>
+            <div>name: {name}</div>
+        </div>
+    );
+}
+
+export default User;
