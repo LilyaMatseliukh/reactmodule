5a6f99297845daf995f4e4efc083ff48a2883bb7
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-11-03 12:39:06 +0200

    lesson2

--- src/components/UsersContainer/Users/Users.js
+++ src/components/UsersContainer/Users/Users.js
@@ -3,10 +3,14 @@ import axios from "axios";
 
 import css from './Users.module.css';
 import User from "../User/User";
+import {axiosService} from "../../../services/axiosService";
+import {userService} from "../../../services/userService";
+import UserDetails from "../../UserDetails/UserDetails";
 
 const Users = () => {
 
     const [users, setUsers] = useState([]);
+    const [userDetails, setUserDetails] = useState(null);
 
     useEffect(() => {
         // fetch('https://jsonplaceholder.typicode.com/users')
@@ -17,14 +21,36 @@ const Users = () => {
         //     .then(res => res.data)
         //     .then(value => setUsers(value))
 
-        axios.get('https://jsonplaceholder.typicode.com/users')
-            .then(({data}) => setUsers(data))
+        // axios.get('https://jsonplaceholder.typicode.com/users')
+        //     .then(({data}) => setUsers(data))
+
+        // axiosService.get('/users')
+        //     .then(({data}) => setUsers(data))
+
+        userService.getAll().then(({data}) => setUsers(data))
     }, [])
 
+    // const click = (userId) => {
+    //     console.log(userId)
+    // }
+
+    const click = async (userId) => {
+        const {data} = await userService.getById(userId);
+        // console.log(data);
+        setUserDetails(data);
+    }
+
     return (
-        <div className={css.Users}>
+        <div>
+            <div className={css.Users}>
+                {
+                    users.map(user => <User key={user.id} user={user} click={click}/>)
+                }
+            </div>
+
             {
-                users.map(user => <User key={user.id} user={user}/>)
+                // userDetails ? <UserDetails userDetails={userDetails}/> : <h1>Not selected</h1>
+                userDetails && <UserDetails userDetails={userDetails}/>
             }
         </div>
     );
---
+++ src/constants/urls.js
@@ -0,0 +1,10 @@
+const baseURL = 'https://jsonplaceholder.typicode.com';
+const users = '/users';
+const urls = {
+    users: {
+        base: users,
+        byId: (id) => `${users}/${id}`
+    }
+}
+
+export {baseURL, urls};
