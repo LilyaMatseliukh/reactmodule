946fcaea79441a2a3a3be0530e7dc7f2376f27fb
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-11-03 12:57:42 +0200

    homework 2.1

--- src/components/PostContainer/PostsComponent/PostsComponent.js
+++ src/components/PostContainer/PostsComponent/PostsComponent.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 
 import PostComponent from "../PostComponent/PostComponent";
+import css from './PostsComponent.module.css';
 
 const PostsComponent = () => {
     const [posts, setPosts] = useState([]);
@@ -10,7 +11,7 @@ const PostsComponent = () => {
     }, [])
 
     return (
-        <div>
+        <div className={css.Post}>
             {
                 posts.map(post => <PostComponent key={post.id} post={post}/>)
             }
