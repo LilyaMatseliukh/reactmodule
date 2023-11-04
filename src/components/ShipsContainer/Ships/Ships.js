4a4dd3869b1d559ae6a89b43f78b87389a99281e
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-11-04 13:22:00 +0200

    homework 2.2

--- src/components/ShipsContainer/Ships/Ships.js
+++ src/components/ShipsContainer/Ships/Ships.js
@@ -1,13 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import Ship from "../Ship/Ship";
+import {axiosInstance} from "../../../services/ship.api.service";
 
 const Ships = () => {
     const [ships, setShips] = useState([]);
 
     useEffect(() => {
-        fetch('https://api.spacexdata.com/v3/launches/')
-            .then(value => value.json())
-            .then(value => setShips(value))
+        // fetch('https://api.spacexdata.com/v3/launches/')
+        //     .then(value => value.json())
+        //     .then(value => setShips(value))
+
     }, [])
 
     const filteredShipsArray = ships.filter(ship => ship.launch_year !== '2020');
