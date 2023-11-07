b8a236f1b4c68dd510a7a0093378c4746229ccb7
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-11-07 18:58:38 +0200

    homework 2 review

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const App = () => {
-  return (
-      <div>
-          App
-      </div>
-  );
+
+    const [characters, setCharacter] = useState([]);
+
+    let x = 0; // 1 2
+
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/character')
+            .then(value => value.json())
+            .then(({results}) => {
+                // console.log(value.results)
+
+                // const {results} = value;
+                // console.log(results);
+
+                setCharacter(results);
+                console.log('.');
+            });
+    }, [x]);
+
+    return (
+        <div>
+            {/*{*/}
+            {/*    characters.map(character => (<div key={character.id}>{character.name} <p>{character?.asd?.xxx}</p></div>))*/}
+            {/*}*/}
+
+            {
+                characters.map(character => (
+                    <div key={character.id}>
+                        {character.name}
+                        {character.asd && <p>{character.asd.xxx}</p>}
+                    </div>
+                ))
+            }
+        </div>
+    );
 };
 
 export {App};
