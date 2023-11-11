a5f94b033613c18fefd1ad53ebbecb39d8c7eda8
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-11-11 13:47:18 +0200

    lesson 1&2 consultation

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,5 +5,6 @@ const userService = {
     getAll: () => axiosService.get(urls.users.users),
     create: (user) => axiosService.post(urls.users.users, user),
     getById: (id) => axiosService.get(urls.users.users + `/${id}`),
-    deleteById: (id) => axiosService.delete(urls.users.users + `/${id}`)
-}
+    deleteById: (id) => axiosService.delete(urls.users.users + `/${id}`),
+    sorted: (order) => axiosService.get('/users/sorted')
+};
