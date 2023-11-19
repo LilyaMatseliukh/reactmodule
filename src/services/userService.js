3ed8acb1d5b05d15003fa4de57aed5ec2069024b
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-11-19 18:11:12 +0200

    homework 3.1.1

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,9 +1,9 @@
 import {axiosService} from "./axiosService";
 import {urls} from "../constants/urls";
 
-const usersService = {
+const userService = {
     getAll: () => axiosService.get(urls.users),
     create: (user) => axiosService.post(urls.users, user)
 };
 
-export {usersService};
+export {userService};
