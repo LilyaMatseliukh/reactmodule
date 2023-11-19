db721e1c25118eadb417465733828d0144f630cc
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-11-19 17:58:42 +0200

    lesson 3.3

--- src/validators/carValidator.js
+++ src/validators/carValidator.js
@@ -4,10 +4,11 @@ const carValidator = Joi.object({
     brand: Joi.string().pattern(/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/).required().messages({
         'string.pattern.base': 'Only letters, min 1 max 20 characters'
     }),
-    price: Joi.number().min(0).max(1_000_000).required().messages({
-        'number.min': 'min 0',
-        'number.max': 'max 1000000'
-    }),
+    // price: Joi.number().min(0).max(1_000_000).required().messages({
+    //     'number.min': 'min 0',
+    //     'number.max': 'max 1000000'
+    // }),
+    price: Joi.number().min(0).max(1_000_000).required(),
     year: Joi.number().min(1990).max(new Date().getFullYear()).required()
 })
 
