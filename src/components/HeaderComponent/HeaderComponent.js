89c349b9988ee037a862094801b50dbd0e4016d8
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-11-25 01:26:18 +0200

    homework 1

--- src/components/HeaderComponent/HeaderComponent.js
+++ src/components/HeaderComponent/HeaderComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
-import css from './Header.module.css'
 import {Link} from "react-router-dom";
+
+import css from './HeaderComponent.module.css'
 
 const HeaderComponent = () => {
     return (
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,11 @@ import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 
 const routes = createBrowserRouter([
-    {path: '', element: <MainLayout/>}
+    {path: '', element: <MainLayout/>, children: [
+            {path: 'todos'},
+            {path: 'albums'},
+            {path: 'comments'}
+        ]}
 ]);
 
 export {routes};
