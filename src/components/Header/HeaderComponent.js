e75c7ef4b58a34b9bd5a872aafc7de4397ebd00f
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-11-25 01:07:24 +0200

    homework 1

---
+++ src/components/Header/HeaderComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import css from './HeaderComponent.css'
+
+const HeaderComponent = () => {
+    return (
+        <div className={css.Header}>
+
+        </div>
+    );
+};
+
+export default HeaderComponent;
