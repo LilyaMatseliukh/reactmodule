a8e7291d5d718eb178174d09f4c0fe3533e2b617
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-11-25 16:05:09 +0200

    homework 1

--- src/components/header-component/HeaderComponent.js
+++ src/components/header-component/HeaderComponent.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {Link, NavLink} from "react-router-dom";
+import {Link, NavLink, useNavigate} from "react-router-dom";
 
 import css from './HeaderComponent.module.css'
 
 const HeaderComponent = () => {
+    const navigate = useNavigate();
+
     return (
         <div>
             <header className={css.Header}>
@@ -11,6 +13,8 @@ const HeaderComponent = () => {
                 <NavLink to={'albums'}>albums</NavLink>
                 <NavLink to={'comments'}>comments</NavLink>
             </header>
+            <button onClick={() => navigate(-1)}>prev page</button>
+            <button onClick={() => navigate(1)}>next page</button>
         </div>
     );
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,21 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 
 import MainLayout from "../layouts/MainLayout";
 import TodosPage from "../pages/todos-page/TodosPage";
 import AlbumsPage from "../pages/albums-page/AlbumsPage";
 import CommentsPage from "../pages/comments-page/CommentsPage";
 import PostsPage from "../pages/posts-page/PostsPage";
+import ErrorPage from "../pages/error-page/ErrorPage";
 
 const routes = createBrowserRouter([
     {
         path: '',
         element: <MainLayout/>,
+        errorElement: <ErrorPage/>,
         children: [
+            {
+                index: true, element: <Navigate to={'todos'}/>
+            },
             {
                 path: 'todos',
                 element: <TodosPage/>
