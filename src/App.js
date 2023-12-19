d7d3863085aab7e5f18be9668eeda127cd25b064
LilyaMatseliukh <lilyamatselioukh@gmail.com>
2023-12-19 19:54:49 +0200

    homework 6.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Component from "./components/component";
 
 const App = () => {
   return (
       <div>
-          App
+          <Component/>
       </div>
   );
 };
---
+++ src/components/component.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+import css from './component.module.css';
+
+const Component = () => {
+    return (
+        // <div className={css.Main}>
+        //     <div className={css.Part}>Name</div>
+        //     <div className={css.Part}>Name</div>
+        //     <div className={css.Part}>Name</div>
+        //     <div className={css.Part}>Name</div>
+        //     <div className={css.Part}>Name</div>
+        //     <div className={css.Part}>Name</div>
+        //     <div className={css.Part}>Name</div>
+        //     <div className={css.Part}>Name</div>
+        // </div>
+
+        // <table>
+        //     <tr>
+        //         <td>Комірка 1</td>
+        //         <td>Комірка 2</td>
+        //         <td>Комірка 3</td>
+        //     </tr>
+        //     <tr>
+        //         <td>Комірка 4</td>
+        //         <td>Комірка 5</td>
+        //         <td>Комірка 6</td>
+        //     </tr>
+        // </table>
+
+        <table>
+            <td>Комірка 1</td>
+            <td>Комірка 2</td>
+            <td>Комірка 3</td>
+            <td>Комірка 3</td>
+            <td>Комірка 3</td>
+            <td>Комірка 3</td>
+            <td>Комірка 3</td>
+        </table>
+
+        // <table>
+        //     <caption>A summary of top sites with both paid and free course</caption>
+        //     <thead>
+        //     <tr>
+        //         <th className="primary" scope="col">Websites</th>
+        //         <th scope="col">Certification</th>
+        //         <th scope="col">Tuition</th>
+        //         <th scope="col">Top Courses</th>
+        //     </tr>
+        //     </thead>
+        //     <tbody>
+        //     <tr>
+        //         <th scope="row">Freecode Camp</th>
+        //         <td>Yes</td>
+        //         <td>Free</td>
+        //         <td>Web Development and Design</td>
+        //     </tr>
+        //     <tr>
+        //         <th scope="row">Udemy</th>
+        //         <td>Yes</td>
+        //         <td>Paid</td>
+        //         <td>Python and Machine Learning</td>
+        //     </tr>
+        //     <tr>
+        //         <th scope="row">Coursera</th>
+        //         <td>Yes</td>
+        //         <td>Free and Paid</td>
+        //         <td>IoT and AI</td>
+        //     </tr>
+        //     </tbody>
+        //     <tfoot>
+        //     <tr>
+        //         <th>Total Courses</th>
+        //         <td colSpan="4"><i>What are you waiting for? Get Started now for free</i></td>
+        //     </tr>
+        //     </tfoot>
+        // </table>
+    );
+};
+
+export default Component;
